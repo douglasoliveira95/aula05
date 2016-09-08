c7d83ee6bfab13a1730399836a7bc3899dc86f93
Douglas Oliveira <douglinhas_uchiha@hotmail.com>
2016-09-08 20:12:41 -0300

    Update in sum

--- sum/sum.js
+++ sum/sum.js
@@ -1,8 +1,20 @@
-var n1 = parseFloat(process.argv[2]);
-var n2 = parseFloat(process.argv[3]);
+var numberA = parseFloat(process.argv[2]);
+var numberB = parseFloat(process.argv[3]);
 
 
-console.log("Total: " + (n1 + n2));
+console.log("Numbers: " + getNumbers());
+
+function getNumbers() {
+    var numbers = [];
+    for (var i=2; i<process.argv.length; i++) {
+        var stringNumber = process.argv[i];
+        var number = parseFloat(stringNumber);
+        if (!isNaN(number)) {
+            numbers.push(number);
+        }
+    }
+    return numbers;
+}
 
 
 
